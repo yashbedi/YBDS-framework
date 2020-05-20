b42b613e5969cef8011378e7bd4a0e4f03a89eb8
yash <yash.bedi@paytm.com>
2020-05-20 22:33:16 +0530

    stack---

--- YBDS/YBDS/Stack.swift
+++ YBDS/YBDS/Stack.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+// Vanila verion of Generic Queue without using inbuild api's
+// MARK: Stack Data Structure
+
+public struct Stack<T> {
+    private var stackArr: [T]
+    var isEmpty : Bool {
+        return stackArr.count == 0
+    }
+    var tos : T? {
+        if isEmpty {
+            debugPrint("Stack is Empty")
+            return nil
+        }else{
+            return stackArr.last
+        }
+    }
+    init() { stackArr = [] }
+    mutating func push(_ data: T){ stackArr.append(data) }
+    mutating func pop() {
+        if isEmpty {
+            debugPrint("Stack is Empty")
+            return
+        }
+        stackArr.removeLast()
+    }
+}
